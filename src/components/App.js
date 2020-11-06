e787ae447fc3e17c5fadc28138a3e63806e88766
Billy Downing <wldowning@gmail.com>
2020-11-05 19:24:23 -0700

    added streaming components and routes in app file

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-const PageOne = () => {
-  return (
-    <div>
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <button>Click me!</button>
-      <Link to="/">Navigate To Page One</Link>
-    </div>
-  );
-};
-
-const App = () => {
+  const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path='/' exact component={PageOne} />
-          <Route path='/pagetwo' component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
