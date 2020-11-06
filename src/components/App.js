b76b23b1b64c5d5d4a8b299c3a02cb72f9c77407
Billy Downing <wldowning@gmail.com>
2020-11-05 18:37:16 -0700

    added routing with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <button>Click me!</button>
+      <Link to="/">Navigate To Page One</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={PageOne} />
+          <Route path='/pagetwo' component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
